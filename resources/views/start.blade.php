652349775e142b5c26ce62fccafd99998cf6c659
cyberkeiler <toby@terramultimedia.de>
2016-11-05 19:22:56 +0100

    startpage

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.dashboard')
 @section('page_heading','Openscad Portal')
 @section('section')
-
-
-
+  @section ('inside_panel_title', 'Welcome')
+  @section ('inside_panel_body')
+  <p>Welcome to the Openscad Portal. This is a private platform for makers, collaborating for creating and optimizing custom objects</p>
+  <p>This site is under heavy developement, so do not expect anything to work the way it should!</p>
+  @endsection
+  @include('widgets.panel', array('header'=>true, 'as'=>'inside'))
 @stop
