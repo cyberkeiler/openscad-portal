623c4e31dc45598e7e75a54ac84774b8ca1297c0
cyberkeiler <toby@terramultimedia.de>
2016-11-05 19:06:15 +0100

    Introduced Homecontroller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function logout()
+    {
+      auth::logout();
+      return view('start');
+    }
+}
