79f71c414f08882a26b7211c1dca3cb62fddf0cf
cyberkeiler <toby@terramultimedia.de>
2016-11-09 20:37:37 +0100

    Created german language files

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwörter müssen mindestens sechs Zeichen lang sein und korrekt Wiederholt werden.',
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben dir einen Passwort-Reset Link zugemailed!',
+    'token' => 'Dieser Passwort-Reset Token ist ungültig.',
+    'user' => "Wir können keinen Nutzer mit dieser Mailadresse finden.",
+
+];
